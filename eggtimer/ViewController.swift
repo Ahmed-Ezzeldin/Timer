d405dde7ba46f0fbc5582ea0cf5b2c33b5cad5b4
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-04-28 10:09:55 +0200

    Initial Commit

---
+++ eggtimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eggtimer
+//
+//  Created by Cloud Secrets on 28/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
