6fe117f967c9347a20ed095eb43903c1177786c8
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-07 16:49:54 +0200

    selected timer

--- eggtimer/ViewController.swift
+++ eggtimer/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let eggsTimes = ["Soft": 5 , "Medium": 7 , "Hard": 12];
+    @IBOutlet weak var selectedTime: UILabel!
+    let eggsTimes = ["Soft": 300 , "Medium":420 , "Hard": 720];
+    var secondsRemaining = 60;
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,16 +19,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onEggPressed(_ sender: UIButton) {
-        switch sender.tag{
-        case 1: print("Soft");
-        case 2: print("Medium");
-        case 3: print("Hard");
-        default:
-            print("Error");
-            
-            
-        }
+        
+                switch sender.tag{
+                case 1:
+                    selectedTime.text = "\(String(describing: sender.titleLabel!.text!)) \(eggsTimes[sender.titleLabel!.text!]!)";
+                case 2:    print(eggsTimes["\(String(describing: sender.titleLabel!.text!))"]!);
+                    selectedTime.text = "\(String(describing: sender.titleLabel!.text!)) \(eggsTimes[sender.titleLabel!.text!]!)";
+                case 3:     print(eggsTimes["\(String(describing: sender.titleLabel!.text!))"]!);
+                    selectedTime.text = "\(String(describing: sender.titleLabel!.text!)) \(eggsTimes[sender.titleLabel!.text!]!)";
+                default:
+                    print("Error");
+                }
+//        ======================================================================
+        //        var hardness : String? = sender.titleLabel.text;
+//        var hardness : String? =    sender.titleLabel!.text!;
+        
+        //        print("\(eggsTimes[hardness]!)");
+        
+//        secondsRemaining = eggsTimes["\(String(describing: sender.titleLabel!.text!))"]!
+//        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
+//    @objc func updateTimer(){
+//        if(secondsRemaining>0){
+//            printContent("Seconds remaining -----> \(secondsRemaining)")
+//            secondsRemaining -= 1;
+//
+//        }
+//    }
     
 }
 
