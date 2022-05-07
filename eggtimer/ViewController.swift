4d557529d074649baeb48c97d2ffcbd37a87f011
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-07 11:58:26 +0200

    add time dictionary

--- eggtimer/ViewController.swift
+++ eggtimer/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let eggsTimes = ["Soft": 5 , "Medium": 7 , "Hard": 12];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onEggPressed(_ sender: UIButton) {
+        switch sender.tag{
+        case 1: print("Soft");
+        case 2: print("Medium");
+        case 3: print("Hard");
+        default:
+            print("Error");
+            
+            
+        }
+    }
+    
 }
 
